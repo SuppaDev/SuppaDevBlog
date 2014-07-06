3eb9a9a602c3571e3a0a1ea77987449964075028
SuppaDev <asuppa@outlook.com>
2014-07-06 12:00:13 -0400

    initial
    Blog Post 1

---
+++ AdControl1.cs
@@ -0,0 +1,6 @@
+ <UI:AdControl x:Name="MsAdControl" 
+			   ApplicationId="5e4f65765cf41cd3d"
+			   AdUnitId="103" 
+               HorizontalAlignment="Left" 
+               Height="80" VerticalAlignment="Top" Width="480" 
+               />
---
+++ AdControl2.cs
@@ -0,0 +1,8 @@
+UI:AdControl x:Name="MsAdControl" 
+			 ApplicationId="5e4f63ba-" 
+			 AdUnitId="1073" 
+             HorizontalAlignment="Left" 
+             Height="80" VerticalAlignment="Top"
+			 Width="480" 
+             ErrorOccurred="Ad_ErrorOccurred"
+			 AdRefreshed="_AdRefreshed"/>

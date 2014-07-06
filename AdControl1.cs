c36621d72e6e78dd2743eeac4e2287a33d123a22
SuppaDev <asuppa@outlook.com>
2014-07-06 12:08:19 -0400

    Update AdControl1.cs

--- AdControl1.cs
+++ AdControl1.cs
@@ -1,7 +1,8 @@
  <UI:AdControl x:Name="MsAdControl" 
-			   ApplicationId="5e4f65765cf41cd3d"
-			   AdUnitId="103" 
+	       ApplicationId="5e4f65765cf41cd3d"
+	       AdUnitId="103" 
                HorizontalAlignment="Left" 
-               Height="80" VerticalAlignment="Top" Width="480" 
+               Height="80" VerticalAlignment="Top" 
+               Width="480" 
                />
 			   

7aa4b5db18432c5b245fd0b6bef2e23ff2718e36
SuppaDev <asuppa@outlook.com>
2014-07-06 12:08:52 -0400

    Update AdControl2.cs

--- AdControl2.cs
+++ AdControl2.cs
@@ -1,9 +1,9 @@
 UI:AdControl x:Name="MsAdControl" 
-			 ApplicationId="5e4f63ba-" 
-			 AdUnitId="1073" 
+	     ApplicationId="5e4f63ba-" 
+	     AdUnitId="1073" 
              HorizontalAlignment="Left" 
              Height="80" VerticalAlignment="Top"
-			 Width="480" 
+	     Width="480" 
              ErrorOccurred="Ad_ErrorOccurred"
-			 AdRefreshed="_AdRefreshed"/>
+	     AdRefreshed="_AdRefreshed"/>
 			 
